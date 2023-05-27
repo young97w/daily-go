c164f6d2e6d27d141830ef7765116ac6cc769bef
young97w <yangyuansky@outlook.com>
2023-05-27 20:20:45 +0800

    mock to unit test

--- cache/redis_test.go
+++ cache/redis_test.go
@@ -1,40 +1,137 @@
 package cache
 
-//func TestRedisCache_Get(t *testing.T) {
-//	testCases := []struct {
-//		name    string
-//		mock    func(ctrl *gomock.Controller) redis.Cmdable
-//		key     string
-//		wantErr error
-//		wantVal string
-//	}{
-//		{
-//			name: "get value",
-//			mock: func(ctrl *gomock.Controller) redis.Cmdable {
-//				cmd := mocks.NewMockCmdable(ctrl)
-//
-//				str := redis.NewStringCmd(context.Background())
-//				str.SetVal("value1")
-//
-//				cmd.EXPECT().Get(context.Background(), "key1").Return(str)
-//				return cmd
-//			},
-//		},
-//	}
-//
-//	for _, tc := range testCases {
-//		t.Run(tc.name, func(t *testing.T) {
-//			ctrl := gomock.NewController(t)
-//			defer ctrl.Finish()
-//
-//			c := NewRedisCache(tc.mock(ctrl))
-//
-//			val, err := c.Get(context.Background(), tc.key)
-//			assert.Equal(t, tc.wantErr, err)
-//			if err != nil {
-//				return
-//			}
-//			assert.Equal(t, tc.wantVal, val)
-//		})
-//	}
-//}
+import (
+	"context"
+	"fmt"
+	"geektime/cache/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestRedisCache_SetU(t *testing.T) {
+	testCases := []struct {
+		name       string
+		mock       func(ctrl *gomock.Controller) redis.Cmdable
+		key        string
+		value      string
+		expiration time.Duration
+		wantErr    error
+	}{
+		{
+			name: "set ok",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+
+				status := redis.NewStatusCmd(context.Background())
+				status.SetVal("OK")
+
+				cmd.EXPECT().Set(context.Background(), "key1", "value1", time.Second).Return(status)
+				return cmd
+			},
+			expiration: time.Second,
+			key:        "key1",
+			value:      "value1",
+		},
+		{
+			name: "time out",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				status := redis.NewStatusCmd(context.Background())
+				status.SetErr(context.DeadlineExceeded)
+				cmd.EXPECT().
+					Set(context.Background(), "key1", "value1", time.Second).
+					Return(status)
+				return cmd
+			},
+			key:        "key1",
+			value:      "value1",
+			expiration: time.Second,
+			wantErr:    context.DeadlineExceeded,
+		},
+		{
+			name: "unexpected msg",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				status := redis.NewStatusCmd(context.Background())
+				status.SetVal("NO OK")
+				cmd.EXPECT().
+					Set(context.Background(), "key1", "value1", time.Second).
+					Return(status)
+				return cmd
+			},
+			key:        "key1",
+			value:      "value1",
+			expiration: time.Second,
+			wantErr:    fmt.Errorf("%w, 返回信息 %s", errFailedToSetCache, "NO OK"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			c := NewRedisCache(tc.mock(ctrl))
+			err := c.Set(context.Background(), tc.key, tc.value, tc.expiration)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
+
+func TestRedisCache_Get(t *testing.T) {
+	testCases := []struct {
+		name    string
+		mock    func(ctrl *gomock.Controller) redis.Cmdable
+		key     string
+		wantErr error
+		wantVal string
+	}{
+		{
+			name: "get value",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+
+				str := redis.NewStringCmd(context.Background())
+				str.SetVal("value1")
+
+				cmd.EXPECT().Get(context.Background(), "key1").Return(str)
+				return cmd
+			},
+			key:     "key1",
+			wantVal: "value1",
+		},
+		{
+			name: "timeout",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+
+				str := redis.NewStringCmd(context.Background())
+				str.SetErr(context.DeadlineExceeded)
+
+				cmd.EXPECT().Get(context.Background(), "key1").Return(str)
+				return cmd
+			},
+			key:     "key1",
+			wantErr: context.DeadlineExceeded,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			c := NewRedisCache(tc.mock(ctrl))
+
+			val, err := c.Get(context.Background(), tc.key)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantVal, val)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/golang/mock v1.4.4
+	github.com/google/uuid v1.3.0
 	github.com/gotomicro/ekit v0.0.6
 	github.com/hashicorp/golang-lru v0.5.1
 	github.com/mattn/go-sqlite3 v1.14.16
@@ -18,6 +19,7 @@ require (
 	go.opentelemetry.io/otel/exporters/zipkin v1.14.0
 	go.opentelemetry.io/otel/sdk v1.14.0
 	go.opentelemetry.io/otel/trace v1.14.0
+	golang.org/x/sync v0.1.0
 )
 
 require (
@@ -28,7 +30,6 @@ require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/openzipkin/zipkin-go v0.4.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
